bb201b92b82462c30b9034d4b6249fbdb23b067d
cindykhin <cicispm@gmail.com>
2025-02-01 12:31:27 -0500

    Added functions for game status and game mode on arduino

--- arduino/arduinoCode.ino
+++ arduino/arduinoCode.ino
@@ -1,14 +1,32 @@
-// RUN IN ARDUINO IDE
-
-// RGB LED
+/*
+    RGB LED: THIS IS A VISUAL CUE ON THE WINNER
+*/
 #define BLUE 11
 #define GREEN 12
 #define RED 13
 
-// Buttons
+/*  
+    LED: THIS IS TO SET THE GAME STATUS
+*/
+#define WAIT 22
+#define READY 24
+#define SINGLE_PLAYER 26
+#define MULTI_PLAYER 28
+
+/*
+    BUTTONS: THESE ARE USED TO GAGE REACTION TIME
+*/
+
+
+// GLOBAL VARIALES, RECEIVED FROM SERIAL CONNECTION
+int g_GameMode = 3;
+bool g_isReady = false;
+
 
 void setup()
 {
+  Serial.begin(9600);
+
   // RGB setup
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
@@ -17,8 +35,20 @@ void setup()
   digitalWrite(GREEN, LOW);
   digitalWrite(BLUE, LOW);
 
+  // LED setup
+  pinMode(WAIT, OUTPUT);
+  pinMode(READY, OUTPUT);
+  pinMode(SINGLE_PLAYER, OUTPUT);
+  pinMode(MULTI_PLAYER, OUTPUT);
+
+  digitalWrite(WAIT, LOW);
+  digitalWrite(READY, LOW);
+  digitalWrite(SINGLE_PLAYER, LOW);
+  digitalWrite(MULTI_PLAYER, LOW);
+
   // Buttons setup
 }
+
 
 void setColourRgb(int iRed, int iGreen, int iBlue)
 {
@@ -32,10 +62,68 @@ void clearRgb()
   setColourRgb(0, 0, 0);
 }
 
+void setGameStatusReady(bool bIsReady)
+{
+  if (bIsReady)
+  {
+    digitalWrite(READY, HIGH);
+    digitalWrite(WAIT, LOW);
+    Serial.print("ready\n");
+  }
+  else
+  {
+    digitalWrite(READY, LOW);
+    digitalWrite(WAIT, HIGH);
+    Serial.print("wait\n");
+  }
+}
+
+void setPlayerMode(int iMode)
+{
+  if (iMode == 1)
+  {
+    digitalWrite(SINGLE_PLAYER, HIGH);
+    digitalWrite(MULTI_PLAYER, LOW);
+
+    Serial.print("SinglePlayer\n");
+  }
+  if (iMode == 2)
+  {
+    digitalWrite(SINGLE_PLAYER, LOW);
+    digitalWrite(MULTI_PLAYER, HIGH);
+
+    Serial.print("MultiPlayer\n");
+  }
+  if (iMode == 3)
+  {
+    digitalWrite(SINGLE_PLAYER, LOW);
+    digitalWrite(MULTI_PLAYER, LOW);
+    setGameStatusReady(false);
+
+    Serial.print("Waiting\n");
+  }
+}
+
 // main loop
 void loop()
 {
-  delay(1000);
+  delay(5000);
   setColourRgb(0, 255, 0);
+  setPlayerMode(g_GameMode);
+
+  delay(5000);
+  setGameStatusReady(g_isReady);
+
+  delay(5000);
+  g_GameMode = 1;
+  setPlayerMode(g_GameMode);
+
+  delay(5000);
+  g_GameMode = 2;
+  setPlayerMode(g_GameMode);
+
+  delay(5000);
+  g_isReady = true;
+  setGameStatusReady(g_isReady);
 }
 
